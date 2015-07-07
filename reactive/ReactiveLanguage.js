21267e0d430d2f731c7d7c65ac7070899e4e8749
Kevin Noppe <kevinnoppe@hotmail.com>
2015-07-07 20:31:26 +0200

    Remove obsolete functions Add reactive function names for scan and flatMap

--- reactive/ReactiveLanguage.js
+++ reactive/ReactiveLanguage.js
@@ -1,14 +1,16 @@
 ﻿var ReactiveLanguage = function (language) {
     language = "language";
-    functions = {};
-    functionCalls = {};
-    nodeExecution = {};
+    //functions = {};
+    //functionCalls = {};
+    //nodeExecution = {};
     models = {};
 };
 
 // All available language constructs
 ReactiveLanguage.empty = "empty";
 ReactiveLanguage.map = "map";
+ReactiveLanguage.flatMap = "flatMap";
+ReactiveLanguage.scan = "scan";
 ReactiveLanguage.filter = "filter";
 ReactiveLanguage.zip = "zip";
 ReactiveLanguage.fromEvent = "fromEvent";
@@ -17,28 +19,28 @@ ReactiveLanguage.subscription = "subscribe";
 ReactiveLanguage.consoleOutput = "consoleOutput";
 
 // The list with all functions that can be executed.
-ReactiveLanguage.prototype.functions = {};
+//ReactiveLanguage.prototype.functions = {};
 
 // The list with all functions that can be called for code creation.
-ReactiveLanguage.prototype.functionCalls = {};
+//ReactiveLanguage.prototype.functionCalls = {};
 
 /**
  * Get the reactive function to execute
  * 
  * @param name The identifier of the language construct.
  */
-ReactiveLanguage.prototype.getFunction = function (name) {
-    return functions[name];
-};
+//ReactiveLanguage.prototype.getFunction = function (name) {
+//    return functions[name];
+//};
 
 /**
  * Get the call to the reactive function for use in code generation.
  * 
  * @param name The identifier of the language construct.
  */
-ReactiveLanguage.prototype.getFunctionCall = function (name) {
-    return functionCalls[name];
-};
+//ReactiveLanguage.prototype.getFunctionCall = function (name) {
+//    return functionCalls[name];
+//};
 
 /**
  * Get the code necessary to execute the reactive function in its node.
